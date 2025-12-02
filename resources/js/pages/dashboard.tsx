1632378caf6a8908e5e77c54e9b816bd2cb35c87
Deniz Şahin <denizsahin567@gmail.com>
2025-12-02 11:18:22 +0300

    fix: Dashboard compile error

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,49 +1,49 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
-import AppLayout from '@/layouts/app-layout';
-import { dashboard } from '@/routes';
-import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react';
+// import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+// import AppLayout from '@/layouts/app-layout';
+// import { dashboard } from '@/routes';
+// import { type BreadcrumbItem } from '@/types';
+// import { Head } from '@inertiajs/react';
 
-const breadcrumbs: BreadcrumbItem[] = [
-    {
-        title: 'Dashboard',
-        href: dashboard().url,
-    },
-];
+// const breadcrumbs: BreadcrumbItem[] = [
+//     {
+//         title: 'Dashboard',
+//         href: dashboard().url,
+//     },
+// ];
 
-export default function Dashboard() {
-    return (
-        <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                </div>
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    {/* <Carousel>
-                        <CarouselContent>
-                            <CarouselItem>...</CarouselItem>
-                            {cats.map((cat) => (
-                                <CarouselItem key={cat.id} className="flex h-full flex-col items-center justify-center bg-muted">
-                                    <div className="text-center">
-                                        <h3 className="mb-2 text-2xl font-bold">{cat.name}</h3>
-                                        <p className="text-sm text-muted-foreground">Some other text</p></div>
-                                </CarouselItem>
-                                    ),)}
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel> */}
-                </div>
-            </div>
-        </AppLayout>
-    );
-}
+// export default function Dashboard() {
+//     return (
+//         <AppLayout breadcrumbs={breadcrumbs}>
+//             <Head title="Dashboard" />
+//             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+//                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+//                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+//                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+//                     </div>
+//                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+//                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+//                     </div>
+//                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+//                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+//                     </div>
+//                 </div>
+//                 <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+//                     {/* <Carousel>
+//                         <CarouselContent>
+//                             <CarouselItem>...</CarouselItem>
+//                             {cats.map((cat) => (
+//                                 <CarouselItem key={cat.id} className="flex h-full flex-col items-center justify-center bg-muted">
+//                                     <div className="text-center">
+//                                         <h3 className="mb-2 text-2xl font-bold">{cat.name}</h3>
+//                                         <p className="text-sm text-muted-foreground">Some other text</p></div>
+//                                 </CarouselItem>
+//                                     ),)}
+//                         </CarouselContent>
+//                         <CarouselPrevious />
+//                         <CarouselNext />
+//                     </Carousel> */}
+//                 </div>
+//             </div>
+//         </AppLayout>
+//     );
+// }
